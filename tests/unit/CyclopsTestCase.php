1e86374a1c83da5df663ab23aa198b62e7306074
Catherine Doherty <catherine@gcdtech.com>
2018-09-20 12:47:43 +0100

    adding assertThrowsException

--- tests/unit/CyclopsTestCase.php
+++ tests/unit/CyclopsTestCase.php
@@ -3,8 +3,29 @@
 namespace Gcd\Cyclops\Tests\unit;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_Exception;
 
 class CyclopsTestCase extends TestCase
 {
+    public static function assertThrowsException(
+        $expectedException,
+        callable $callable,
+        $message = '',
+        $expectedExceptionMessage = ''
+    ) {
+        $thrown = false;
+        try {
+            $callable();
+        } catch (PHPUnit_Framework_Exception $exception) {
+            throw $exception;
+        } catch (\Exception $ex) {
+            $thrown = true;
+            self::assertInstanceOf($expectedException, $ex, $message);
+            if (!empty($expectedExceptionMessage)) {
+                self::assertEquals($expectedExceptionMessage, $ex->getMessage(), $message);
+            }
+        }
 
+        self::assertTrue($thrown, $message !== null ? $message : "Expected a {$expectedException} to be thrown");
+    }
 }
