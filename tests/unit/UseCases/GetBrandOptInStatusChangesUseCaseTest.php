f531fb864b90198419d311daa395361132fc1529
Catherine Doherty <catherine@gcdtech.com>
2018-09-25 17:21:54 +0100

    tests for get brand opt in status changes use case

---
+++ tests/unit/UseCases/GetBrandOptInStatusChangesUseCaseTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Gcd\Cyclops\Tests\unit\UseCases;
+
+use Codeception\Stub;
+use Gcd\Cyclops\Entities\CustomerEntity;
+use Gcd\Cyclops\Entities\CyclopsIdentityEntity;
+use Gcd\Cyclops\Services\CyclopsService;
+use Gcd\Cyclops\Tests\unit\CyclopsTestCase;
+use Gcd\Cyclops\UseCases\GetBrandOptInStatusChangesUseCase;
+
+class GetBrandOptInStatusChangesUseCaseTest extends CyclopsTestCase
+{
+    private $optIn = true;
+
+    public function testBrandOptInStatusChangesReturned()
+    {
+        $cyclopsId = 12;
+        $service = Stub::make(CyclopsService::class, [
+            'createCustomer' => function (CyclopsIdentityEntity $identityEntity) use (&$count): CustomerEntity {
+                $identityEntity->id = $count++;
+                $customer = new CustomerEntity();
+                $customer->identity = $identityEntity;
+                return $customer;
+            },
+            'getBrandOptInStatusChanges' => function (string $startingDate) use ($cyclopsId) {
+                return [
+                    'data' => [
+                        'cyclopsId' => $cyclopsId,
+                        'optIn' => $this->optIn,
+                        'brandId' => '15',
+                        'optIn_datetime' => '2018-09-25T15:15:00+0000',
+                    ],
+                ];
+            },
+        ]);
+
+        $id = new CyclopsIdentityEntity();
+        $id->email = 'test@hotmail.com';
+        $id->id = $cyclopsId;
+        $service->createCustomer($id);
+
+        $useCase = new GetBrandOptInStatusChangesUseCase($service);
+        $startingDate = new \DateTime();
+        $response = $useCase->execute($startingDate->format('Y-m-d\TH:i:s\Z'));
+
+        verify($response['data']['cyclopsId'])->equals($cyclopsId);
+        verify($response['data']['optIn'])->true();
+
+        $this->optIn = false;
+        $response = $useCase->execute($startingDate->format('Y-m-d\TH:i:s\Z'));
+        verify($response['data']['optIn'])->false();
+    }
+}
