0a1d56b396e42c5db5b3473cc0f2855a4ebd2055
Catherine Doherty <catherine@gcdtech.com>
2018-10-02 12:04:33 +0100

    push delete and stale commands

---
+++ src/Commands/PushDeletedToCyclopsCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Gcd\Cyclops\Commands;
+
+use Gcd\Cyclops\UseCases\PushDeletedToCyclopsUseCase;
+
+abstract class PushDeletedToCyclopsCommand extends PushStateToCyclopsCommand
+{
+    final function executeUseCase()
+    {
+        $pushUseCase = new PushDeletedToCyclopsUseCase($this->getService());
+        $pushUseCase->execute($this->getList());
+    }
+}
