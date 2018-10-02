52d91ef44cdb5ba0a0c04fd65ac83ea1acd106f9
Catherine Doherty <catherine@gcdtech.com>
2018-10-02 09:59:05 +0100

    testing pull changes from cyclops use case

---
+++ tests/unit/UseCases/PullChangesFromCyclopsUseCaseTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Gcd\Cyclops\Tests\unit\UseCases;
+
+use Codeception\Stub;
+use Gcd\Cyclops\Entities\CustomerEntity;
+use Gcd\Cyclops\Entities\CyclopsIdentityEntity;
+use Gcd\Cyclops\Services\CyclopsService;
+use Gcd\Cyclops\Tests\unit\CyclopsTestCase;
+use Gcd\Cyclops\UseCases\PullChangesFromCyclopsUseCase;
+
+class PullChangesFromCyclopsUseCaseTest extends CyclopsTestCase
+{
+    public function testChangesPulledFromCyclops()
+    {
+        $cyclopsId = 12;
+
+        $service = Stub::make(CyclopsService::class, [
+            'createCustomer' => function (CyclopsIdentityEntity $identityEntity) use (&$count): CustomerEntity {
+                $identityEntity->id = $count++;
+                $customer = new CustomerEntity();
+                $customer->identity = $identityEntity;
+                return $customer;
+            },
+            'loadCustomer' => function (CyclopsIdentityEntity $identityEntity) use (&$count): CustomerEntity {
+                $customer = new CustomerEntity();
+                $customer->identity = $identityEntity;
+                return $customer;
+            },
+            'getBrandOptInStatusChanges' => function (\DateTime $startingDate) use ($cyclopsId) {
+                return [
+                    'data' => [
+                    ],
+                ];
+            }
+        ]);
+
+        $count = 0;
+        $useCase = new PullChangesFromCyclopsUseCase($service);
+        $useCase->execute(new \DateTime('2018-08-01 00:00:00'), function() use (&$count) {
+            $count++;
+        });
+
+        verify($count)->equals(1);
+    }
+}
