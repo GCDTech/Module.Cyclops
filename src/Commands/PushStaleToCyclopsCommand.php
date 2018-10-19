597ddad57141625dfa8c93bc497322eaee6b05ae
Catherine Doherty <catherine@gcdtech.com>
2018-10-19 16:40:08 +0100

    adding an event handler for member creation from queue processor

--- src/Commands/PushStaleToCyclopsCommand.php
+++ src/Commands/PushStaleToCyclopsCommand.php
@@ -9,6 +9,8 @@ abstract class PushStaleToCyclopsCommand extends PushStateToCyclopsCommand
     final function executeUseCase()
     {
         $pushUseCase = new PushStaleToCyclopsUseCase($this->getService());
-        $pushUseCase->execute($this->getList());
+        $pushUseCase->execute($this->getList(), $this->onCustomerCreated());
     }
+
+    abstract protected function onCustomerCreated(): callable;
 }
