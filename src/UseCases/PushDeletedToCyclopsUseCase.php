f6b9a52e8a5e3b9b22f175aa0d859acfd286b105
Molly <mballantine@gcdtech.com>
2023-09-06 14:39:35 +0100

    Throw new CyclopsException for deleted user not found
    Throw a CyclopsException when a CustomerNotFoundException is caught so that we can retry the request and/or send the failure email.
    Also added some clarifying comments.

--- src/UseCases/PushDeletedToCyclopsUseCase.php
+++ src/UseCases/PushDeletedToCyclopsUseCase.php
@@ -24,12 +24,22 @@ class PushDeletedToCyclopsUseCase
         foreach ($list->items as $item) {
             try {
                 $this->cyclopsService->deleteCustomer($item->identity, $item->timestamp);
+
                 if ($onCustomerDeleted !== null) {
+                    // Set CyclopsQueue item to sent
                     $onCustomerDeleted($item, true);
                 }
             } catch (CustomerNotFoundException $exception) {
                 if ($onCustomerDeleted !== null) {
+                    // Set CyclopsQueue item to not sent
                     $onCustomerDeleted($item, false);
+
+                    // Throw new exception to trigger retry request and/or failure email
+                    throw new CyclopsException(
+                        "404 Customer Not Found and queue item not sent",
+                        404,
+                        $exception
+                    );
                 }
             }
         }
